a4e76a5f75872f30b8e32d0e100f6eaeb1ae475d
ChoiByeol <honey333888@naver.com>
2020-10-08 20:59:37 +0900

    컴포넌트 등록

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
-
+    <TodoHeader></TodoHeader>
+    <TodoFooter></TodoFooter>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
-export default {
+import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
 
+export default {
+    components: {
+      // '컴포넌트 이름': '컴포넌트 내용'
+      'TodoHeader': TodoHeader,
+      'TodoFooter': TodoFooter,
+      'TodoInput': TodoInput,
+      'TodoList': TodoFooter,
+  }
 }
 </script>
 
