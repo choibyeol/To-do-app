96afc763ae3ef060e3f6b567974936693b987a8c
ChoiByeol <honey333888@naver.com>
2020-10-09 00:51:52 +0900

    TodoHeader

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-      header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped> /* 싱글 파일 컴포넌트 안에서만 유효한 스타일 속성으로 변환 */
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
