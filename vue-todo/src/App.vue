3dc79f3ad8575aad042a639e80c82b8e5e00cae3
ChoiByeol <honey333888@naver.com>
2020-10-14 22:09:39 +0900

    할 일 저장 기능

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoFooter></TodoFooter>
     <TodoInput></TodoInput>
     <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
 import TodoHeader from './components/TodoHeader.vue'
-import TodoFooter from './components/TodoFooter.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
     components: {
       // '컴포넌트 이름': '컴포넌트 내용'
       'TodoHeader': TodoHeader,
-      'TodoFooter': TodoFooter,
       'TodoInput': TodoInput,
-      'TodoList': TodoFooter,
+      'TodoList': TodoList,
+      'TodoFooter': TodoFooter,
+      
   }
 }
 </script>
